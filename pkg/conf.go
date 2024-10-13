3d032c5f7ff52dba3c1a019568b1c62db1dddf4f
Joel Santos <joe@joesantos.io>
2024-10-13 20:47:43 +0100

    setup config proxies

--- pkg/conf.go
+++ pkg/conf.go
@@ -3,3 +3,21 @@ package pkg
 import "github.com/iamajoe/auth/internal/conf"
 
 type GlobalConfiguration = conf.GlobalConfiguration
+type APIConfiguration = conf.APIConfiguration
+type DBConfiguration = conf.DBConfiguration
+type ProviderConfiguration = conf.ProviderConfiguration
+type LoggingConfig = conf.LoggingConfig
+type ProfilerConfig = conf.ProfilerConfig
+type TracingConfig = conf.TracingConfig
+type MetricsConfig = conf.MetricsConfig
+type SMTPConfiguration = conf.SMTPConfiguration
+type PasswordConfiguration = conf.PasswordConfiguration
+type JWTConfiguration = conf.JWTConfiguration
+type MailerConfiguration = conf.MailerConfiguration
+type SmsProviderConfiguration = conf.SmsProviderConfiguration
+type HookConfiguration = conf.HookConfiguration
+type SecurityConfiguration = conf.SecurityConfiguration
+type SessionsConfiguration = conf.SessionsConfiguration
+type MFAConfiguration = conf.MFAConfiguration
+type SAMLConfiguration = conf.SAMLConfiguration
+type CORSConfiguration = conf.CORSConfiguration
